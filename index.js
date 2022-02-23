7072ffe834605362a7dfeab15e292e2007b2f2d3
kunk45 <mithusin410@gmail.com>
2022-02-23 11:18:37 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+/* function myfun(){
+
+    const node=document.getElementById("mylist2").lastElementChild;
+    document.getElementById("mylist1").appendChild(node);
+} */
+
+
+/* function  myfun(){
+    const node=document.createElement("li");
+    const textnode=document.createTextNode("black coffee");
+    node.appendChild(textnode);
+    document.getElementById("mylist1").appendChild(node);
+} */
+
+
+
+/* function myfun(){
+    const newnode=document.createElement("li");
+    const textnode=document.createTextNode("pineapple");
+    newnode.appendChild(textnode);
+    const list=document.getElementById("mylist1");
+    list.insertBefore(newnode,list.children[0]);
+} */
+
+/* function myfun(){
+    const node=document.createElement("li");
+    const text=document.createTextNode("colddrink");
+    node.appendChild(text);
+    const list=document.getElementById("mylist1");
+    list.insertBefore(node,list.children[1]);
+} */
+
+
+
+/* function myfun(){
+    const node=document.getElementById("mylist2").lastElementChild;
+   
+    
+    const list=document.getElementById("mylist1");
+    list.insertBefore(node,null);
+
+} */
+
+
+
+
